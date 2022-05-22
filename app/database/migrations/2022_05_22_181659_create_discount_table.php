85e8329055b762beb67a181192aad1952602171c
Amir <amir@Amirs-MacBook-Pro.local>
2022-05-22 23:33:29 +0430

    First commit

---
+++ app/database/migrations/2022_05_22_181659_create_discount_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discount', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->dateTime('startTime');
+            $table->dateTime('endTime');
+            $table->string('discountType');
+            $table->string('code')->unique();
+            $table->float('amount');
+            $table->integer('limite');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discount');
+    }
+};
